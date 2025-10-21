c55b767ffcaa41f5ad509ad1b3258df1a48f00b9
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-21 19:03:47 +0900

    feat(Application): InputView, RacingGame, Referee, OutputView 순서로 전체 기능 호출

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,37 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.application.RacingGame;
+import racingcar.application.Referee;
+import racingcar.common.NameParser;
+import racingcar.domain.Car;
+import racingcar.domain.MovementChecker;
+import racingcar.domain.RandomMovementChecker;
+import racingcar.ui.InputView;
+import racingcar.ui.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NameParser nameParser = new NameParser();
+        InputView inputView = new InputView(nameParser);
+        List<String> carNames = inputView.carNames();
+        int repeatCount = inputView.repeatCount();
+
+        List<Car> cars = new ArrayList<>();
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        MovementChecker movementChecker = new RandomMovementChecker();
+        RacingGame racingGame = new RacingGame(cars, movementChecker);
+        cars = racingGame.race(repeatCount);
+
+        Referee referee = new Referee(cars);
+        List<Car> winner = referee.findWinner();
+
+        OutputView outputView = new OutputView();
+        outputView.finalResult(winner);
     }
 }
