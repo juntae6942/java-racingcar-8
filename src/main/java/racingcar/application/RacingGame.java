9d3720580db1f9d56d00717f53c34037226f5ab9
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-21 18:25:01 +0900

    feat(RacingGame): 경주 진행 후 각 라운드 상태 출력 기능 구현

---
+++ src/main/java/racingcar/application/RacingGame.java
@@ -0,0 +1,37 @@
+package racingcar.application;
+
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.domain.MovementChecker;
+
+public class RacingGame {
+
+    private final MovementChecker movementChecker;
+    private final List<Car> cars;
+    private int repeatCount;
+
+    public RacingGame(List<Car> cars, MovementChecker movementChecker) {
+        this.cars = cars;
+        this.movementChecker = movementChecker;
+    }
+
+    public List<Car> race(int repeatCount) {
+        for (int i = 0; i < repeatCount; i++) {
+            moveAllCars();
+            showRoundStatus();
+        }
+        return cars;
+    }
+
+    private void moveAllCars() {
+        for (Car car : cars) {
+            car.move(movementChecker);
+        }
+    }
+
+    private void showRoundStatus() {
+        for (Car car : cars) {
+            System.out.println(car);
+        }
+    }
+}
