a177fa1b915a4559e085af118dc6b3de448b07f7
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-21 18:51:59 +0900

    refactor(OutputView): finalResult의 파라미터 인자 String을 Car로 수정

--- src/main/java/racingcar/ui/OutputView.java
+++ src/main/java/racingcar/ui/OutputView.java
@@ -1,14 +1,16 @@
 package racingcar.ui;
 
 import java.util.List;
+import racingcar.domain.Car;
 
 public class OutputView {
 
-    public void finalResult(List<String> names) {
-        if (names.size() > 1) {
+    public void finalResult(List<Car> cars) {
+        if (cars.size() > 1) {
+            List<String> names = cars.stream().map(Car::name).toList();
             System.out.println(String.join(", ", names));
         } else {
-            System.out.println(names.getFirst());
+            System.out.println(cars.getFirst().name());
         }
     }
 }
