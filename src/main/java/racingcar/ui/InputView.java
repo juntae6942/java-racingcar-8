5fdfc7d568116c32226fb6730ce9bbd231dd0199
potato <65760583+juntae6942@users.noreply.github.com>
2025-10-22 16:08:06 +0900

    fix(InputView): 입력에 대한 안내문 추가

--- src/main/java/racingcar/ui/InputView.java
+++ src/main/java/racingcar/ui/InputView.java
@@ -16,6 +16,7 @@ public class InputView {
     }
 
     public List<String> carNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
         List<String> names = nameParser.parse(input);
         names.forEach(NameValidator::validateName);
@@ -23,6 +24,7 @@ public class InputView {
     }
 
     public int repeatCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine();
         NumberValidator.validateNumber(input);
         return Integer.parseInt(input);
